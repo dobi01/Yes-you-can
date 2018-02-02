727bfa28f27f821f79fbb00d070c4fd92f19a90b
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-02-02 20:51:50 +0100

    Add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,47 @@
 const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-        output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+  entry: './src/index.js',
+    output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules"
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: "/node_modules"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
-                        }
-                    }
-                ]
+    {
+      test: /\.css$/,
+      use:
+        [
+          { loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
             }
+          }
         ]
-    }
+      }
+    ]
+  },
+  plugins:
+    [
+      new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+      }),
+      new webpack.optimize.UglifyJsPlugin(),
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    ]
 };
